57e2cafda141b69a6a86c7008f1f1dd694d548fd
jwilyandi19 <jasonwilyandi@yahoo.co.id>
2017-10-25 07:35:53 +0700

    tambah fungsi player1

--- soal2.c
+++ soal2.c
@@ -9,7 +9,54 @@ int hole1[17];
 int hole2[17];
 pthread_t tid1;
 pthread_t tid2;
-int score1=0,score2=0,have1=16,have2=16;	
+int score1=0,score2=0,have1=16,have2=16;
+int bet;
+int picked;
+int stat;
+
+int status_returner() {
+	if(score1==10 || score2==10 || have1==0 || have2==0) return 3;
+}
+
+void *play1 (void *arg) {
+	
+	while(1) {
+		printf("%s's turn, your score: %d\n",player1,score1);
+		printf("Let's pick\n");
+		stat = 0;
+		scanf("%d",&bet);
+
+		while(bet--) {
+			
+			scanf("%d",&picked);
+			if(hole1[picked]==0) have1 = have1-1;
+			hole1[picked] = 1;
+		}
+		stat = 1;
+		while(stat==1 || stat==3) {
+			if(stat==3) return NULL;
+		}
+		printf("%s's turn, let's guess\n",player1);
+
+		scanf("%d",&picked);
+
+		if(hole2[picked]==0) {
+			printf("%s: +1\n",player1);
+			score1++;
+		}
+		else {
+			printf("%s: +1\n",player2);
+			score2++;
+		}
+
+		if(status_returner()==3) {
+			stat = 3;
+			return NULL;
+		}
+	}
+}
+		
+			
 
 
 int main () {
@@ -22,4 +69,10 @@ int main () {
 		hole1[i] = 0;
 		hole2[i] = 0;
 	}
+
+	pthread_create(&(tid1),NULL,&play1,NULL);
+	//pthread_create(&(tid2),NULL,&play2,NULL);
+
+	pthread_join(tid1,NULL);
+	//pthread_join(tid2,NULL);
 }
